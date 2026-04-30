878fe6f3d3b9fe5c9cd156a0e9e3edb482915c62
Tripti <trishukharole1331@gmail.com>
2026-05-01 02:10:10 +0530

    Add gitignore and remove unnecessary files

--- backend/app.py
+++ backend/app.py
@@ -256,4 +256,5 @@ def get_history(user_id):
 
 if __name__ == "__main__":
     get_model()
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get("PORT", 8000))
+    app.run(host="0.0.0.0", port=port)
