54012ddcc28ac8c7823a52337e320c665260d007
Tripti <trishukharole1331@gmail.com>
2026-05-02 18:29:24 +0530

    Switch to Keras model format

--- backend/convert_model.py
+++ backend/convert_model.py
@@ -1,6 +1,4 @@
 import tensorflow as tf
 
-model = tf.keras.models.load_model("model/lsd_model.h5", compile=False)
-model.save("model/lsd_model_tf213.h5")
-
-print("Model converted successfully")
+model = tf.keras.models.load_model("model/lsd_model_tf213.h5", compile=False)
+model.save("model/lsd_model.keras")
