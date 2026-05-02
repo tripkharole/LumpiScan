1436df0dc26782f0e4d4d3e4b63ad96b82087f4c
Tripti <trishukharole1331@gmail.com>
2026-05-02 19:30:30 +0530

    Fix dependency conflicts

--- backend/convert_model.py
+++ backend/convert_model.py
@@ -1,4 +1,12 @@
 import tensorflow as tf
 
-model = tf.keras.models.load_model("model/lsd_model_tf213.h5", compile=False)
-model.save("model/lsd_model.keras")
+model = tf.keras.models.load_model("backend/model/lsd_model_tf213.h5", compile=False)
+
+model_config = model.get_config()
+new_model = tf.keras.Model.from_config(model_config)
+
+new_model.set_weights(model.get_weights())
+
+new_model.save("backend/model/lsd_model_clean.keras")
+
+print("Clean model saved successfully")
